38c24836ab992b9068afe904a4fff50f995f73a7
ksudyn <ksudyn@student.42madrid.com>
2024-12-16 13:57:52 +0100

    n4

--- nivel4/ft_itoa.c
+++ nivel4/ft_itoa.c
@@ -10,4 +10,51 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 
+int lennbr(int nbr)
+{
+	int i;
+
+	if (nbr == 0)
+		return (1);
+	i = 0;
+	while (nbr != 0)
+	{
+		nbr /= 10;
+		i++;
+	}
+	return (i);
+}
+
+int sign(int nbr)
+{
+	if (nbr < 0)
+		return (-nbr);
+	return (nbr);
+}
+
+char	*ft_itoa(int nbr)
+{
+	char *num;
+	int len;
+	int i;
+
+	len = lennbr(nbr);
+	i = 0;
+	if (nbr < 0)
+		i++;
+	len = len + i;
+	num = (char *)malloc((len + 1) * sizeof(char));
+	if (!num)
+		return (NULL);
+	num[0] = '-';
+	num[len] = '\0';
+	while ((len - 1) >= i)
+	{
+		num[len - 1] = sign(nbr % 10) + '0';
+		nbr /= 10;
+		len--;
+	}
+	return (num);
+}
--- nivel4/ft_list_remove_if.c
+++ nivel4/ft_list_remove_if.c
@@ -10,4 +10,30 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+typedef struct      s_list
+{
+    struct s_list   *next;
+    void            *data;
+}                   t_list;
 
+void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
+{
+    if(!begin_list || ! *begin_list)
+    {
+        return;
+    }
+
+    t_list *cont = *begin_list;
+
+    if(cmp(cont->data, data_ref) == 0)
+    {
+		*begin_list = cont->next;
+		free(cont);
+		ft_list_remove_if(begin_list, data_ref, cmp);
+	}
+	else
+	{
+		cont = *begin_list;
+		ft_list_remove_if(&cont->next, data_ref, cmp);
+	}
+}
