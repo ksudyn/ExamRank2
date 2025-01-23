2a7f036c323a7d5a53efa848ee146991eba32886
ksudyn <ksudyn@student.42madrid.com>
2025-01-23 17:37:01 +0100

    ex

--- nivel4/ft_list_foreach.c
+++ nivel4/ft_list_foreach.c
@@ -24,7 +24,7 @@ void ft_list_foreach(t_list *begin_list, void (*f)(void *))
     }
     while (begin_list)
     {
-        f( begin_list->data);
+        (*f)( begin_list->data);
         begin_list = begin_list->next;
     }
 }
