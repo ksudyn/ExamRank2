3af18e6924eb2642d63976f8d06512cf163df61d
ksudyn <ksudyn@student.42madrid.com>
2025-02-27 15:35:54 +0100

    nuevos gnl

--- Rank_03/get_next_line.c
+++ Rank_03/get_next_line.c
@@ -141,3 +141,50 @@ char *get_next_line(int fd)
     remainder = save_remainder(remainder);
     return line;
 }
+//..................................................//
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+char	*get_next_line(int fd)
+{
+	char	*ret = malloc(9999);
+	char	c;
+	int		i = 0;
+	int		cpt = 0;
+
+	if (fd < 0)
+		return (NULL);
+	while ((cpt = read(fd, &c, 1)) > 0)
+	{
+		ret[i] = c;
+		i++;
+		if (c == '\n')
+			break ;
+	}
+	if (i == 0 || cpt < 0)
+	{
+		free(ret);
+		return (NULL);
+	}
+	ret[i] = '\0';
+	return (ret);
+}
+
+int		main(int argc, char **argv)
+{
+	int 	fd = 0;
+	char	*line;
+
+	if (argc > 1)
+		fd = open(argv[1], O_RDONLY);
+	line = get_next_line(fd);
+	while (line != NULL)
+	{
+		printf("line |%s", line);
+		free(line);
+		line = get_next_line(fd);
+	}
+	return (0);
+}
